7a6bdf3f4cb403a08abbf0b7a5a476cb1e4d12ee
VicBocanegra25 <victor.bocanegra.e@gmail.com>
2023-03-14 22:41:11 -0600

    Adding two .js files. These are the exercises performed during the class of 08/03/2023. The file named calentamiento.js is the solution to the number of coins problem and the one called ejercicio_object.js is a solution to the problem of finding and returning the object type of a variable (returning a message if a variable passed IS an object or a primitive

---
+++ ejercicio_object.js
@@ -0,0 +1,32 @@
+/* Ejercicio 1 en JavaScript: Implementar una expresión para verificar si un valor es un objeto
+
+Tomar en cuenta que: 
+typeof(null) -> object
+
+function: verifyType(myVar):
+params: 
+    myVar: a variable that is passed to the function, the user inputs the value
+returns: the type of the parameter passed to the function
+*/
+
+function verifyType(myVar){
+    if (typeof(myVar) === 'object'){
+        return "This variable is an object.";
+    }
+    return "This variable is a primitive";
+
+}
+
+let myVar = "one";
+console.log(verifyType(myVar));
+myVar = "1";
+console.log(verifyType(myVar));
+myVar = 1;
+console.log(verifyType(myVar));
+myVar = true;
+console.log(verifyType(myVar));
+myVar = [1, 2];
+console.log(verifyType(myVar));
+myVar = {"myVar" : 1}
+console.log(verifyType(myVar));
+
