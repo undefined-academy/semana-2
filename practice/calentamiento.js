b222c788f9515292c2d362d24a37ac542b6ad974
Joshua_Arfa <soondevjosh@gmail.com>
2023-03-20 17:43:37 -0600

    adding the pre practice

---
+++ practice/calentamiento.js
@@ -0,0 +1,12 @@
+let hugo = 25;
+
+let paco = 2 * hugo;
+let luis = 10;
+
+let total = hugo + paco + luis;
+
+if( total === 85 ) {
+    console.log("Hugo: Q." + hugo + ".00");
+    console.log("Paco: Q." + paco + ".00");
+    console.log("Luis: Q." + luis + ".00");
+}
