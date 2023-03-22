6ee66fe4883c6d5ff61764b68e9804fa8ee95b44
Juan <js.sanchez130@gmail.com>
2023-03-22 10:24:57 -0500

    Prevent form default behavior

---
+++ student-cvs/juanssanchez-6134/main.js
@@ -0,0 +1,9 @@
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Hola mundo");
+}
+
+const $form =
+    document.querySelector("form");
+
+$form.addEventListener("submit", handleSubmit);
