52d6587c2c738d3e156065903d41e09a9592b65c
GastonR <gastonrrodriguez@gmail.com>
2023-04-05 15:51:13 -0300

    Add

--- student-cvs/gaston-0280/contacto/index.mjs
+++ student-cvs/gaston-0280/contacto/index.mjs
@@ -1,1 +1,7 @@
-console.log("Hello word");
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log("hola mundo");
+}
+
+const $form = document.querySelector("form");
+$form.addEventListener("submit",handleSubmit);
