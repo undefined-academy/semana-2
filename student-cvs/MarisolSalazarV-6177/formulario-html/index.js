aafae80fd967107419c40347ab00652a4eea4ad6
matias <matiiojj@gmail.com>
2023-04-09 22:33:53 -0300

    Actualizar directorios

--- student-cvs/MarisolSalazarV-6177/formulario-html/index.js
+++
@@ -1,6 +0,0 @@
-const ContactForm = document.querySelector('#FormContact');
-
-ContactForm.addEventListener('submit', function(event){
-    event.preventDefault();
-})
-
--- student-cvs/Mau-5427/main.js
+++
@@ -1,7 +0,0 @@
-const d = document;
-
-d.addEventListener("DOMContentLoaded", (e) => {
-  d.addEventListener("submit", (e) => {
-    e.preventDefault();
-  })
-})
