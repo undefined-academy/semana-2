9f38b1fc8d277225a7f02983f2827db6999854b0
luismdev <morais.luism.dev@gmail.com>
2023-05-02 09:39:10 +0200

    Init_02.05.2023

--- student-cvs/EverJM-6591/js/code.js
+++
@@ -1,6 +0,0 @@
-const btnEnviar = document.querySelector("#btn-enviar");
-btnEnviar.addEventListener("click",noCargar);
-function noCargar(event){
-    event.preventDefault();
-    alert("Aun no se puede enviar, muchas gracias :)");
-}
--- student-cvs/Noa_S-5163/js/main.js
+++
@@ -1,6 +0,0 @@
-const formEl = document.querySelector('#form');
-
-formEl.addEventListener('submit', (e) => {
-  e.preventDefault();
-  alert('Message sent!!');
-});
