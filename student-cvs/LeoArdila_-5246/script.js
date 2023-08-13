053f265e6d7b2ccad8f416606f306ddd94817a1b
LeoArdila94 <leoardila94@gmail.com>
2023-08-13 11:47:38 -0500

    Carpeta HV LeoArdila_

---
+++ student-cvs/LeoArdila_-5246/script.js
@@ -0,0 +1,4 @@
+               document.getElementById("formulario").addEventListener("submit", function (event) {
+                   event.preventDefault();
+                   alert("Buen intento de enviar eventos... pero lo tuve que detener :(");
+               });
